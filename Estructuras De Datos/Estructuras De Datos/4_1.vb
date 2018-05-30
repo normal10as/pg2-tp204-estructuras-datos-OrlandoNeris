c89105da1e717773858a55bd9bedac47511beffe
OrlandoNeris <orlandoneris24@gmail.com>
2018-05-30 01:37:30 -0300

    tt

---
+++ Estructuras De Datos/Estructuras De Datos/4_1.vb
@@ -0,0 +1,37 @@
+﻿Module _4_1
+
+    '.Elaborar un algoritmo que permita cargar 5 números en un arreglo de 5 elementos;
+    ' calcular la media y luego imprimir cada elemento del arreglo y la desviación que tiene respecto a la media.
+    'La MEDIA se obtiene la sumatoria de los todos elementos del arreglo y se divide por la cantidad de los mismos.
+    ' La DESVIACION de cada elemento se obtiene restándole al elemento la MEDIA.
+    '
+
+    Sub main()
+
+        Dim valores(4) As Integer
+        Dim MEDIA As Integer
+
+        For i = 0 To 4
+            Console.WriteLine("Ingrese {0}° elemento a evaluar: ", i + 1)
+            valores(i) = Console.ReadLine
+            MEDIA += valores(i)
+        Next
+
+        MEDIA = MEDIA / 5
+
+        Console.WriteLine()
+        Console.WriteLine("La media de los elementos ingresados es: " & MEDIA)
+        Console.WriteLine()
+
+        For i = 0 To 4
+            Console.WriteLine("El elemento ingresado en la {0}° posicion tiene una desviacion de: {1} de la MEDIA.", i, valores(i) - MEDIA)
+        Next
+
+        Console.ReadKey()
+
+
+    End Sub
+
+
+
+End Module
---
+++ Estructuras De Datos/Estructuras De Datos/4_2.vb
@@ -0,0 +1,51 @@
+﻿Module _4_2
+
+    'Se tiene tres arrays cargados programáticamente con una lista de productos: 
+    'el primero tiene el código, 
+    'el segundo el nombre
+    'el tercero el precio unitario. 
+    'Repetitivamente se ingresa un código de producto y el programa muestra su descripción y precio, 
+    'luego el usuario ingresa la cantidad con lo que el programa responde calculando el total del producto y el total general
+    '(acumulando los productos anteriores).
+    'Esto sucede hasta que el código sea cero.
+
+    Sub main()
+
+        Dim CodProducto = New UShort() {1, 2, 4, 8}
+        Dim NomProducto = New String() {"Leche", "Pan", "Papa", "Yerba"}
+        Dim PreciUnit = New Single() {1.4, 2, 4.3, 8.1}
+        Dim TotalGeneral As Single = 0
+
+        Dim opcion As Integer
+
+        Do
+            Dim cantidad As UShort
+
+
+            If opcion = 0 Then
+                Exit Do
+            End If
+            For Each codigo As Integer In CodProducto
+                If codigo = opcion Then
+                    Console.WriteLine("Descripcion: " & NomProducto(opcion))
+                    Console.WriteLine("Precio Unitario: " & PreciUnit(opcion))
+                    Console.Write("Ingrese Cantidad: ")
+                    cantidad = Console.ReadLine
+                    TotalGeneral += PreciUnit(opcion) * cantidad
+                    Console.WriteLine("Total del Producto: " & PreciUnit(opcion) * cantidad)
+                    Console.WriteLine("Total General: " & TotalGeneral)
+                End If
+            Next
+            Console.WriteLine("Codigo Inexistente... Intente de nuevo")
+        Loop Until (True)
+
+        Console.ReadKey()
+
+    End Sub
+
+    Function ValidarCodigo(ByRef value As UShort)
+        Console.Write("Ingrese Codigo de Producto: ")
+        value = Console.ReadLine
+        Do
+    End Function
+End Module
